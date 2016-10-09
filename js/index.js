9ab260429416cec399d75c501e07c7b2c8a9aea5
Andre Marcelo-Tanner <andre@enthropia.com>
2016-10-09 12:06:52 +0800

    adding docs from jsdoc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+/**
+ * getChange accepts two parameters (totalPayable and cashPaid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} totalPayable the integer amount (in pennies) to be paid
+ * @param {number} cashPaid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // returns [50, 20, 10, 5]
+ */
 function getChange(totalPayable, cashPaid) {
 	totalPayable = totalPayable || 0;
 	cashPaid = cashPaid || 0;
